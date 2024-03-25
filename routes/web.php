51f3363238e71229452051462c0fbdd8b994869f
mukhandia <felixmukhandia@hmail.com>
2024-03-25 17:59:40 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
-
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth','verified'])->group(function(){
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/note',[NoteController::class,'index'])->name('note.index');
+// Route::get('/note/create',[NoteController::class,'create'])->name('note.create');
+// Route::post('/note',[NoteController::class, 'store'])->name('note.store');
+// Route::get('/note/{id}',[NoteController::class, 'show'])->name('note.show');
+// Route::get('/note/{id}/edit',[NoteController::class, 'edit'])->name('note.edit');
+// Route::put('/note/{id}',[NoteController::class, 'update'])->name('note.update');
+// Route::delete('/note/{id}',[NoteController::class, 'destroy'])->name('note.destroy');
+
+Route::resource('note', NoteController::class);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
